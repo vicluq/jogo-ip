610875e4ab0f4813e31374c63c0b4429677316ff
vicluq <victoria.tewari@gmail.com>
2021-04-24 17:47:46 -0300

    combat system

--- src/main.c
+++ src/main.c
@@ -5,12 +5,13 @@
 int main()
 {
     SetConfigFlags(FLAG_VSYNC_HINT);
-    InitWindow(0, 0, "raylib [core] example - mouse input");
+    InitWindow(0, 0, "Game");
     if (!IsWindowFullscreen())
         ToggleFullscreen();
 
     const int screenWidth = GetScreenWidth();
     const int screenHeight = GetScreenHeight();
+
     SetTargetFPS(60);
 
     // NOTE Camera
@@ -20,13 +21,18 @@ int main()
                        .zoom = 1};
 
     // TODO Load das texturas e player
-    Texture2D background = LoadTexture("../assets/Cave.png");
+    Texture2D background = LoadTexture("./assets/Cave.png");
 
-    Rectangle player1 = {-50, -30, 7.5, 7.5};
-    const float playerSpeed = 5;
+    Rectangle lifeBar = {10, 10, 100, 100};
+
+    Rectangle player1 = {-50, -30, 60, 30};
+    const float playerSpeed = 15;
     int previousX = 0, previousY = 0;
-    
-    Rectangle enemies[50] = {};
+
+    Rectangle weapon = {0, 0, 30, 15};
+
+    Rectangle enemy = {100, 120, 40, 40};
+    // Rectangle enemies[50] = {};
 
     // TODO criar mecânica co player
 
@@ -35,6 +41,7 @@ int main()
     {
         previousY = player1.y;
         previousX = player1.x;
+
         if (IsKeyPressed(KEY_W) || IsKeyDown(KEY_W))
         {
             player1.y -= playerSpeed * 0.3;
@@ -58,23 +65,42 @@ int main()
         //  Camera
         if ((previousY != player1.y) || (previousX != player1.x))
         {
-            camera.target.x += ((player1.x) - camera.target.x) * 0.01;
-            camera.target.y += ((player1.y) - camera.target.y) * 0.015;
+            camera.target.x += ((player1.x) - camera.target.x) * 0.025;
+            camera.target.y += ((player1.y) - camera.target.y) * 0.025;
         }
+
         // Drawing mode
         BeginDrawing();
 
         ClearBackground(RAYWHITE);
-        DrawText("Aqui é o topo", screenWidth / 2, 10, 20, BLUE);
 
+        if (CheckCollisionRecs(weapon, enemy))
+        {
+            DrawText("COLISÃOOOO", screenWidth / 2, 10, 20, RED);
+        }
+
+        // NOTE 2d
         BeginMode2D(camera);
 
-        Vector2 texturePos = {0, 0};
-        DrawTextureEx(background, texturePos, 0, 1.5, WHITE); 
-        
-        DrawText("TEXTOOO", -25, -25, 20, GREEN);
-        
+        DrawTextureEx(background, (Vector2){.x = 0, .y = 0}, 0, 3, WHITE);
+
         DrawRectangleRec(player1, RED);
+        DrawRectangleRec(enemy, BLACK);
+
+        // Combate TODO tentar J
+        if (IsKeyPressed(KEY_K))
+        {
+            weapon.x = player1.x + player1.width;
+            weapon.y = player1.y + 2;
+            weapon.width = 30;
+            weapon.height = 15;
+            DrawRectangleRec(weapon, GREEN);
+        }
+        else
+        {
+            weapon.width = 0;
+            weapon.height = 0;
+        }
 
         EndMode2D();
         EndDrawing();
@@ -82,3 +108,7 @@ int main()
 
     return 0;
 }
+/*
+   TODO Inimigo
+    Movimentação do inimigo similar a câmera
+*/
